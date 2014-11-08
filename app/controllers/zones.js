b0e7f67724f352b28bbc8bf474e99676fda4c9fc
Eric Turner <ericturnerdev@gmail.com>
2014-11-08 09:32:16 +0000

    Some progress

--- app/controllers/zones.js
+++ app/controllers/zones.js
@@ -5,19 +5,48 @@ var ZonesController = Ember.ArrayController.extend({
   actions: {
     parseInput: function(){
       var locationLocal = this.get('locationLocal');
+      var locationRemote = this.get('locationRemote');
       var topAlertBox  = document.getElementById("sandbox");
       Ember.$.ajax({
         type: "GET",
         url: "http://api.zn.ericturnerdev.com?location=" + locationLocal,
         success: function(json) {
           console.log(json);
-          var result = JSON.parse(json).time;
+          var resultLocal = JSON.parse(json).time;
+          /*
           topAlertBox.innerHTML = result;
+          */
         },
         error: function(e) { topAlertBox.innerHTML = "Please enter a valid location.";
                              console.log(e.message);
         }
       });
+      Ember.$.ajax({
+        type: "GET",
+        url: "http://api.zn.ericturnerdev.com?location=" + locationRemote,
+        success: function(json) {
+          console.log(json);
+          var resultRemote = JSON.parse(json).time;
+          /*
+          topAlertBox.innerHTML = result;
+          */
+        },
+        error: function(e) { topAlertBox.innerHTML = "Please enter a valid location.";
+                             console.log(e.message);
+        }
+      });
+
+      var sLocal = resultLocal.split(/[-: ]/);
+      var dLocal = new Date(s[0], s[1]-1, s[2], s[3], s[4], s[5]);
+
+      var sRemote = resultRemote.split(/[-: ]/);
+      var dRemote = new Date(s[0], s[1]-1, s[2], s[3], s[4], s[5]);
+
+      var sEvent = resultEvent.split(/[-: ]/);
+      var dEvent = new Date(s[0], s[1]-1, s[2], s[3], s[4], s[5]);
+
+      var secDiff = dRemote - dLocal;
+      //
     }
   }
 });
