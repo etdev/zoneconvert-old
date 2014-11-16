0f83d8981b845638e17fec25e48c630dea04110a
Eric Turner <ericturnerdev@gmail.com>
2014-11-16 13:52:36 +0000

    Working on controller, unfinished state

--- app/controllers/zones.js
+++ app/controllers/zones.js
@@ -10,19 +10,34 @@ var ZonesController = Ember.ArrayController.extend({
       var targetTime = this.get('locationTarget');
 
       //Fetch local info
-      var localTime = getTime(locationLocal);
+      var localDateTime;
+      getTime(locationLocal, 'local');
+      console.log("localDateTime: " + localDateTime + "\n");
+      var remoteDateTime;
+      getTime(locationRemote, 'remote');
+      console.log("remoteDateTime: " + remoteDateTime + "\n");
+      var localDate = extractDate(localDateTime);
+      console.log("localDate: " + localDate + "\n");
+      var localTime  = extractTime(localDateTime);
       console.log("localTime: " + localTime + "\n");
-      var remoteTime = getTime(locationRemote);
+      var remoteDate = extractDate(remoteDateTime);
+      console.log("remoteDate: " + remoteDate + "\n");
+      var remoteTime  = extractTime(localDateTime);
       console.log("remoteTime: " + remoteTime + "\n");
-      var diffDays = getDayDiff(localTime, remoteTime);
-      console.log("diffDays: " + diffDays + "\n");
-      var diffHours = getHourDiff(localTime, remoteTime);
-      console.log("diffHours: " + diffHours + "\n");
-      var diffMins = getMinuteDiff(localTime, remoteTime);
-      console.log("diffMins: " + diffMins + "\n");
 
+      var hourDiff = getHourDiff(localDateTime, remoteDateTime);
+      var minDiff = getMinDiff(localDateTime, remoteDateTime);
 
-      function getTime(location){
+      //var diff  = getDiff(localTime, remoteTime);
+      //console.log("diff: " + diff + "\n");
+      //var diffDays = getDayDiff(localDate, remoteDate);
+      //console.log("diffDays: " + diffDays + "\n");
+      //var diffHours = getHourDiff(localTime, remoteTime);
+      //console.log("diffHours: " + diffHours + "\n");
+      //var diffMins = getMinuteDiff(localTime, remoteTime);
+      //console.log("diffMins: " + diffMins + "\n");
+
+      function getTime(location, identifier){
         //Fetch remote info
         Ember.$.ajax({
           type: "GET",
@@ -30,35 +45,39 @@ var ZonesController = Ember.ArrayController.extend({
           success: function(json) {
             console.log(json);
             var result = JSON.parse(json).time;
+            if (identifier === 'local'){
+              localDateTime = result;
+            } else { remoteDateTime = result; }
           },
+          async: false,
           error: function(e) {  console.log(e.message);
           }
         });
       }
 
-      function getDiff(localTime, remoteTime){
-        localTime = new Date(localTime);
-        remoteTime = new Date(remoteTime);
-        var diff = localTime.getTime() - remoteTime.getTime();
+      function getDiff(local, remote){
+        local = new Date(local);
+        remote = new Date(remote);
+        var diff = local.getTime() - remote.getTime();
         return diff;
       }
 
-      function getDayDiff(localTime, remoteTime){
-        var diff = getDiff(localTime, remoteTime);
+      function getDayDiff(local, remote){
+        var diff = getDiff(local, remote);
         var diffDays = Math.ceil(diff / (1000*3600*24));
         return diffDays;
       }
 
-      function getHourDiff(localTime, remoteTime){
-        var diff = getDiff(localTime, remoteTime);
-        var diffDays = getDayDiff(localTime, remoteTime);
-        var diffHours = Math.ceil(diff / (1000*3600))-(24*Math.abs(diffDays));
+      function getHourDiff(local, remote){
+        var diff = getDiff(local, remote);
+        var diffDays = getDayDiff(local, remote);
+        var diffHours = Math.ceil(diff / (1000*3600));
         return diffHours;
       }
 
-      function getMinuteDiff(localTime, remoteTime){
-        var diff = getDiff(localTime, remoteTime);
-        var diffHours = getHourDiff(localTime, remoteTime);
+      function getMinuteDiff(local, remote){
+        var diff = getDiff(local, remote);
+        var diffHours = getHourDiff(local, remote);
         var diffMins = Math.ceil(diff / (1000*60))-(60*Math.abs(diffHours));
         return diffMins;
       }
@@ -67,6 +86,22 @@ var ZonesController = Ember.ArrayController.extend({
         var result = new Date(date);
         result.setDate(date.getDate() + numDays);
         return result;
+      }
+
+      function extractDate(timeDate){
+        timeDate = timeDate.trim();
+        var reg = /^([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+        return reg.exec(timeDate)[0];
+      }
+
+      function extractTime(timeDate){
+        timeDate = timeDate.trim();
+        var reg = /([0-9]{2}):([0-9]{2}):([0-9]{2})$/;
+        return reg.exec(timeDate)[0];
+      }
+
+      function getHourDiff(localDateTime, remoteDateTime){
+
       }
 
       function normalizeTime(time){
