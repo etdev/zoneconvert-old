6cdac4a60fa4e68ddf1f4242d332bf7f95576cfd
Eric Turner <ericturnerdev@gmail.com>
2014-11-16 12:59:28 +0000

    Edit CSS

--- app/controllers/zones.js
+++ app/controllers/zones.js
@@ -11,8 +11,16 @@ var ZonesController = Ember.ArrayController.extend({
 
       //Fetch local info
       var localTime = getTime(locationLocal);
+      console.log("localTime: " + localTime + "\n");
       var remoteTime = getTime(locationRemote);
+      console.log("remoteTime: " + remoteTime + "\n");
       var diffDays = getDayDiff(localTime, remoteTime);
+      console.log("diffDays: " + diffDays + "\n");
+      var diffHours = getHourDiff(localTime, remoteTime);
+      console.log("diffHours: " + diffHours + "\n");
+      var diffMins = getMinuteDiff(localTime, remoteTime);
+      console.log("diffMins: " + diffMins + "\n");
+
 
       function getTime(location){
         //Fetch remote info
@@ -28,28 +36,41 @@ var ZonesController = Ember.ArrayController.extend({
         });
       }
 
-      function getDayDiff(localTime, remoteTime){
+      function getDiff(localTime, remoteTime){
         localTime = new Date(localTime);
         remoteTime = new Date(remoteTime);
         var diff = localTime.getTime() - remoteTime.getTime();
+        return diff;
+      }
+
+      function getDayDiff(localTime, remoteTime){
+        var diff = getDiff(localTime, remoteTime);
         var diffDays = Math.ceil(diff / (1000*3600*24));
         return diffDays;
       }
 
       function getHourDiff(localTime, remoteTime){
-        localTime = new Date(localTime);
-        remoteTime = new Date(remoteTime);
-        var diff = localTime.getTime() - remoteTime.getTime();
-        var diffHours = Math.ceil(diff / (1000*3600))%24;
+        var diff = getDiff(localTime, remoteTime);
+        var diffDays = getDayDiff(localTime, remoteTime);
+        var diffHours = Math.ceil(diff / (1000*3600))-(24*Math.abs(diffDays));
         return diffHours;
       }
 
-      function getHourDiff(localTime, remoteTime){
-        localTime = new Date(localTime);
-        remoteTime = new Date(remoteTime);
-        var diff = localTime.getTime() - remoteTime.getTime();
-        var diffHours = Math.ceil(diff / (1000*60))%60;
-        return diffHours;
+      function getMinuteDiff(localTime, remoteTime){
+        var diff = getDiff(localTime, remoteTime);
+        var diffHours = getHourDiff(localTime, remoteTime);
+        var diffMins = Math.ceil(diff / (1000*60))-(60*Math.abs(diffHours));
+        return diffMins;
+      }
+
+      function addDays(date, numDays) {
+        var result = new Date(date);
+        result.setDate(date.getDate() + numDays);
+        return result;
+      }
+
+      function normalizeTime(time){
+
       }
     }
   }
