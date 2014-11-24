7fdc953a62998658606dfde273b7af7238bc09c7
Eric Turner <ericturnerdev@gmail.com>
2014-11-24 07:43:09 +0000

    Update bower/npm versions

--- app/adapters/zone.js
+++ app/adapters/zone.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 var ZoneAdapter = DS.RESTAdapter.extend({
-  host: 'http://api.zoneconvert.com'
+  host: 'http://api.zoneconvert.com:9292'
 });
 
 export default ZoneAdapter;
